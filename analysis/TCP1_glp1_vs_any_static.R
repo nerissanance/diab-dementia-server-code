184e6ebc9dcbf103f25d46b1e29d25db6e538c42
Nerissa Nance <nerissanance@gmail.com>
2022-02-14 14:03:06 -0800

    fixed ties for competing risk between dementia and death

--- analysis/TCP1_glp1_vs_any_static.R
+++ analysis/TCP1_glp1_vs_any_static.R
@@ -67,18 +67,22 @@ for(i in 1:(N_time+1)){
   j=i+1
   d[get(paste0("event_dementia_",i))==1, (paste0("event_dementia_",j)):=1]
   d[get(paste0("event_death_",i))==1, (paste0("event_death_",j)):=1]
-}
-# #edit--when one occurs first, set other to zero so there's no competing event:
-# dementia.nodes<- grep("event_dementia_",names(d))
-# death.nodes<- grep("event_death_",names(d))
-# d[, sum_death :=rowSums(.SD,na.rm=T), .SDcols = death.nodes]
-# d[, sum_dementia :=rowSums(.SD,na.rm=T), .SDcols = dementia.nodes]
-# table(d$sum_death)
-# table(d$event_dementia_10)
-# table(d$sum_dementia)
-# d[sum_death > sum_dementia, (dementia.nodes) := replace(.SD, .SD == 1, 0), .SDcols = dementia.nodes]
-# d[sum_death < sum_dementia, (death.nodes) := replace(.SD, .SD == 1, 0), .SDcols = death.nodes]
-# table(d$event_dementia_10)
+  
+  }
+## UNCOMMENT FOR RUNNING MANUAL COMPETING RISK FIX
+## edit--when one occurs first, set other to zero so there's no competing event:
+dementia.nodes<- grep("event_dementia_",names(d))
+death.nodes<- grep("event_death_",names(d))
+d[, sum_death :=rowSums(.SD,na.rm=T), .SDcols = death.nodes]
+d[, sum_dementia :=rowSums(.SD,na.rm=T), .SDcols = dementia.nodes]
+table(d$sum_death)
+table(d$event_dementia_10)
+table(d$sum_dementia)
+d[sum_death > sum_dementia, (dementia.nodes) := replace(.SD, .SD == 1, 0), .SDcols = dementia.nodes]
+d[sum_death < sum_dementia, (death.nodes) := replace(.SD, .SD == 1, 0), .SDcols = death.nodes]
+# NOTE: decided to prioritize dementia in the event that both death and dementia occur in the same time bin
+d[sum_death== sum_dementia, (death.nodes) := replace(.SD, .SD == 1, 0), .SDcols = death.nodes]
+table(d$event_dementia_10)
 
 #-------------------------------------------------------------------------------
 #  Contrasting if everyone had glp1 versus not
