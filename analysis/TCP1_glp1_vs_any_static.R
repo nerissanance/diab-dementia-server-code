7f1708b148f9bb0f7b2c00604dae9741e98ee38f
n.nance <nerissanance@users.noreply.github.com>
2022-03-07 23:09:06 -0800

    Update TCP1_glp1_vs_any_static.R
    changing TCP1 to start in 2009

--- analysis/TCP1_glp1_vs_any_static.R
+++ analysis/TCP1_glp1_vs_any_static.R
@@ -10,7 +10,7 @@ source(paste0(here::here(),"/0_ltmle_Estimate_update.R"))
 
 # NOTE: edit these hyperparameters or inputs as needed
 
-yr = 2011 #year to start cohort definition (depending on which drugs)
+yr = 2009 #year to start cohort definition (depending on which drugs)
 N_time = 11 #number of time points you want to look at
 ncores = 10 #number of cores to use
 
