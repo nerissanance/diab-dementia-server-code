2c8df9e3dcf7db70c716f3ef8cd5bf51068aa831
andrew.mertens <andrew.mertens3@gmail.com>
2022-04-29 16:18:36 -0700

    rerun

--- simulation study/sim_TCP1_glp1_vs_any_static-all.R
+++ simulation study/sim_TCP1_glp1_vs_any_static-all.R
@@ -73,6 +73,30 @@ difftime(int.end.time, int.start.time, units="mins")
 saveRDS(resdf_noDetQ_tmle, paste0(here::here(),"/data/sim_res_noDetQ_tmle.RDS"))
 
 
+int.start.time <- Sys.time()
+resdf_noDetQ_Qint_ic <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=TRUE, det.Q =FALSE, varmethod = "ic"))
+  return(res)
+}
+int.end.time <- Sys.time()
+difftime(int.end.time, int.start.time, units="mins")
+
+saveRDS(resdf_noDetQ_Qint_ic, paste0(here::here(),"/data/sim_res_noDetQ_Qint_ic.RDS"))
+
+
+int.start.time <- Sys.time()
+resdf_noDetQ_Qint_tmle <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=TRUE, det.Q=FALSE, varmethod = "tmle"))
+  return(res)
+}
+int.end.time <- Sys.time()
+difftime(int.end.time, int.start.time, units="mins")
+
+saveRDS(resdf_noDetQ_Qint_tmle, paste0(here::here(),"/data/sim_res_noDetQ_Qint_tmle.RDS"))
+
+
 
 
 int.start.time <- Sys.time()
