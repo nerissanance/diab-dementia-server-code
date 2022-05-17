9f3b0919b3e48d5687d06c8896e3a31cdcb46d9e
Andrew Mertens <amertens@berkeley.edu>
2022-05-17 08:48:37 -0700

    fig updates

--- simulation study/simulation-figures.R
+++ simulation study/simulation-figures.R
@@ -31,7 +31,7 @@ source(here::here("0_config.R"))
 
 resdf_glm <- readRDS(paste0(here::here(),"/data/sim_res_glm_ic.RDS"))
 resdf_Qint <- readRDS(paste0(here::here(),"/data/sim_res_Qint_ic.RDS"))
-#resdf_ic <- readRDS(paste0(here::here(),"/data/sim_res_ic.RDS"))
+resdf_ic <- readRDS(paste0(here::here(),"/data/sim_res_ic.RDS"))
 resdf_EN_ic <- readRDS(paste0(here::here(),"/data/sim_res_EN.RDS"))
 resdf_EN_Qint <- readRDS(paste0(here::here(),"/data/sim_res_Qint_EN.RDS"))
 resdf_gcomp <- readRDS(paste0(here::here(),"/data/sim_res_gcomp.RDS"))
@@ -52,13 +52,20 @@ resdf_noDetQ_tmle <- readRDS(paste0(here::here(),"/data/sim_res_noDetQ_tmle.RDS"
 resdf_noDetQ_Qint_ic <- readRDS(paste0(here::here(),"/data/sim_res_noDetQ_Qint_ic.RDS"))
 resdf_noDetQ_Qint_tmle <- readRDS(paste0(here::here(),"/data/sim_res_noDetQ_Qint_tmle.RDS"))
 
+resdf_alt_ord <- readRDS(paste0(here::here(),"/data/sim_res_Qint_1se_altOrd.RDS"))
+resdf_rf_gcomp <- readRDS(paste0(here::here(),"/data/sim_res_rf_gcomp.RDS"))
+resdf_ridge <- readRDS(paste0(here::here(),"/data/sim_res_ridge.RDS"))
+resdf_ridge_AUC <- readRDS(paste0(here::here(),"/data/sim_res_ridge_AUC.RDS"))
 
+#add interaction models
+resdf_1se_int <- readRDS(paste0(here::here(),"/data/sim_res_1se_int.RDS"))
+resdf_Qint_1se_int <- readRDS(paste0(here::here(),"/data/sim_res_Qint_1se_int.RDS"))
 
 plotdf <- bind_rows(
   resdf_glm %>% mutate(analysis="glm"),
   resdf_RF %>% mutate(analysis="Random Forest"),
   resdf_Qint %>% mutate(analysis="LASSO, Q-intercept"),
-  #resdf_ic %>% mutate(analysis="LASSO"),
+  resdf_ic %>% mutate(analysis="LASSO"),
   resdf_gcomp %>% mutate(analysis="G-Comp - LASSO"),
   resdf_EN_ic %>% mutate(analysis="EN"),
   resdf_EN_Qint %>% mutate(analysis="EN, Q-intercept"),
@@ -71,8 +78,13 @@ plotdf <- bind_rows(
   resdf_noDetQ_ic %>% mutate(analysis="LASSO no DetQ IC"),
   resdf_noDetQ_tmle %>% mutate(analysis="LASSO no DetQ tmle"),
   resdf_noDetQ_Qint_ic %>% mutate(analysis="LASSO no DetQ Qint IC"),
-  resdf_noDetQ_Qint_tmle %>% mutate(analysis="LASSO no DetQ Qint tmle")
-
+  resdf_noDetQ_Qint_tmle %>% mutate(analysis="LASSO no DetQ Qint tmle"),
+  resdf_alt_ord  %>% mutate(analysis="LASSO Q-intercept 1se, alt order"),
+  resdf_rf_gcomp %>% mutate(analysis="Random Forest, G-comp"),
+  resdf_ridge %>% mutate(analysis="Ridge"),
+  resdf_ridge_AUC %>% mutate(analysis="Ridge-AUC"),
+  resdf_1se %>% mutate(analysis="LASSO 1se interactions"),
+  resdf_Qint_1se %>% mutate(analysis="LASSO Q-intercept 1se interactions")
   )
 
 head(plotdf)
@@ -95,14 +107,15 @@ perf_tab <- plotdf %>% group_by(analysis) %>%
                        summarize(coverage=mean(CI.2.5.<true.RR & true.RR<CI.97.5.)*100,
                                  bias=mean(abs(log(estimate) - log(true.RR))),
                                  variance=mean(std.dev^2),
+                                 bias_std_ratio=mean(abs(log(estimate) - log(true.RR))/std.dev),
                                  mse=variance + bias^2)
-perf_tab %>% arrange(mse)
+res <- perf_tab %>% arrange(mse)
+res
 
 plotdf <- left_join(plotdf,perf_tab, by="analysis") %>% arrange(mse) %>%
           mutate(analysis=factor(analysis, levels=unique(analysis)))
 
 #Try out glmnet with interactions (all, or just with interactions with A)
-# RF with Gcomp=T
 # How many continious with spline terms
 
 
