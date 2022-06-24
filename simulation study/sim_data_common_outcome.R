89de7fe238aa207b99e3ebaec67e9043e1d0391b
andrew.mertens <andrew.mertens3@gmail.com>
2022-06-24 15:19:52 -0700

    update simulations

--- simulation study/sim_data_common_outcome.R
+++ simulation study/sim_data_common_outcome.R
@@ -7,22 +7,19 @@ source(paste0(here::here(),"/synthesizeDD.R"))
 
 cc <- fread(paste0(here::here(),"/data/coefficients.txt"))
 
-#update coefficients
-
+#update coefficients to make dementia outcome more common
 rows <- grep("dementia",cc$var)
-
 for(i in rows){
-  cc[[i,"(Intercept)"]] <- cc[[i,"(Intercept)"]]+4.2
+  cc[[i,"(Intercept)"]] <- cc[[i,"(Intercept)"]]+3.5
 
 }
 
-
 u <- synthesizeDD(cc)
-#d <- sim(u,1000)
-#head(d)
+
 
 sim_list <- NULL
 n<-100
+N_time=10
 
 
 for(i in 1:n){
@@ -40,10 +37,31 @@ for(i in 1:n){
     }
   }
 
+  #note: once events jump to 1, need to remain 1 for remainder of follow up
+  for(k in 1:(N_time+1)){
+    j=k+1
+    d[get(paste0("event_dementia_",k))==1, (paste0("event_dementia_",j)):=1]
+    d[get(paste0("event_death_",k))==1, (paste0("event_death_",j)):=1]
+    d[get(paste0("event_death_",k))==1, (paste0("censor_",i)):=0]
+    d[get(paste0("event_death_",k))==1, (paste0("censor_",j)):=1]
+    d[get(paste0("censor_",k))==1, (paste0("censor_",j)):=1]
+
+  }
+  ## UNCOMMENT FOR RUNNING MANUAL COMPETING RISK FIX
+  ## edit--when one occurs first, set other to zero so there's no competing event:
+  dementia.nodes<- grep("event_dementia_",names(d))
+  death.nodes<- grep("event_death_",names(d))
+  d[, sum_death :=rowSums(.SD,na.rm=T), .SDcols = death.nodes]
+  d[, sum_dementia :=rowSums(.SD,na.rm=T), .SDcols = dementia.nodes]
+
+  d[sum_death > sum_dementia, (dementia.nodes) := replace(.SD, .SD == 1, 0), .SDcols = dementia.nodes]
+  d[sum_death < sum_dementia, (death.nodes) := replace(.SD, .SD == 1, 0), .SDcols = death.nodes]
+  # NOTE: decided to prioritize dementia in the event that both death and dementia occur in the same time bin
+  d[sum_death== sum_dementia, (death.nodes) := replace(.SD, .SD == 1, 0), .SDcols = death.nodes]
+
   sim_list[[i]] <- d
   gc()
 }
-prop.table(table(sim_list[[3]]$event_dementia_1))
-prop.table(table(sim_list[[3]]$event_dementia_12))
+prop.table(table(sim_list[[1]]$event_dementia_1))
+prop.table(table(sim_list[[1]]$event_dementia_11))
 saveRDS(sim_list, paste0(here::here(),"/data/simulated_data_list_commonoutcome.RDS"))
-
