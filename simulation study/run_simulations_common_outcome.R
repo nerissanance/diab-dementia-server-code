637315d3422aa8f5fd11d5a6df8f1198478fb625
andrew.mertens <andrew.mertens3@gmail.com>
2022-07-12 08:08:13 -0700

    rerun

--- simulation study/run_simulations_common_outcome.R
+++ simulation study/run_simulations_common_outcome.R
@@ -36,10 +36,25 @@ saveRDS(resdf_noDetQ_Qint_tmle_common, paste0(here::here(),"/data/sim_res_noDetQ
 int.start.time <- Sys.time()
 resdf_tmle_common <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
   res <- NULL
-  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=FALSE, det.Q=TRUE, varmethod = "tmle"))
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=FALSE, det.Q=TRUE, varmethod = "ic"))
   return(res)
 }
 int.end.time <- Sys.time()
 difftime(int.end.time, int.start.time, units="mins")
 
 saveRDS(resdf_tmle_common, paste0(here::here(),"/data/sim_res_resdf_tmle_common.RDS"))
+
+
+
+
+#No Qint, DetQ
+int.start.time <- Sys.time()
+resdf_tmle_common_tmle <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=FALSE, det.Q=FALSE, varmethod = "tmle"))
+  return(res)
+}
+int.end.time <- Sys.time()
+difftime(int.end.time, int.start.time, units="mins")
+
+saveRDS(resdf_tmle_common_tmle, paste0(here::here(),"/data/sim_res_resdf_tmle_common_tmle.RDS"))
