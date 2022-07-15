87e44292ef75790a05e3cade7bdacc0346e2aa9e
andrew.mertens <andrew.mertens3@gmail.com>
2022-07-15 11:24:23 -0700

    run new sim

---
+++ simulation study/run_simulations_ridge2.R
@@ -0,0 +1,26 @@
+
+rm(list=ls())
+library(here)
+source(here::here("0_config.R"))
+source(paste0(here::here(),"/0_ltmle_Estimate_update.R"))
+source(paste0(here::here(),"/simulation study/0_simulation_functions.R"))
+
+
+
+
+gc()
+d_wide_list <- readRDS(file=here("data/simulated_data_list.RDS"))
+d_wide_list <- d_wide_list[1:100]
+gc()
+
+
+#Ridge
+resdf_noDetQ_Qint_tmle_ridge <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=TRUE, det.Q=FALSE, varmethod = "tmle", override_function=SuperLearner_override_ridge))
+  return(res)
+}
+int.end.time <- Sys.time()
+difftime(int.end.time, int.start.time, units="mins")
+
+saveRDS(resdf_noDetQ_Qint_tmle_ridge, paste0(here::here(),"/data/sim_res_ridge_noDetQ_Qint_tmle3.RDS"))
