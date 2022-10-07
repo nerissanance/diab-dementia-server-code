5f9001f3dd6cb489f852106d02ef7816e51b73af
Andrew Mertens <amertens@berkeley.edu>
2022-10-06 22:11:45 -0700

    rerun sim

--- simulation study/run_simulations_null_no_cens.R
+++ simulation study/run_simulations_null_no_cens.R
@@ -14,8 +14,8 @@ d_wide_list <- readRDS(file=here("data/simulated_data_list_null_no_cens.RDS"))
 d_wide_list <- d_wide_list[1:200]
 gc()
 
-try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[1]], resdf=NULL, Qint=F, det.Q=FALSE, varmethod = "ic",N_time=2))
-res
+# try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[1]], resdf=NULL, Qint=F, det.Q=FALSE, varmethod = "ic",N_time=2))
+# res
 
 #Ntime
 Ntime=2
@@ -79,6 +79,53 @@ saveRDS(resdf_ic, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_ic_T
 # saveRDS(resdf_Qint_ic, paste0(here::here(),"/sim_res/null_no_cens_sim_res_ic_T2.RDS"))
 
 
+#Ntime
+Ntime=4
+
+#primary
+int.start.time <- Sys.time()
+resdf_noDetQ_Qint_tmle_T4 <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[i]], resdf=NULL, Qint=TRUE, det.Q=FALSE, varmethod = "tmle",N_time=4))
+  return(res)
+}
+int.end.time <- Sys.time()
+difftime(int.end.time, int.start.time, units="mins")
+
+saveRDS(resdf_noDetQ_Qint_tmle_T4, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_Qint_tmle_T4.RDS"))
+
+
+int.start.time <- Sys.time()
+resdf_noDetQ_tmle_T4 <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[i]], resdf=NULL, Qint=FALSE, det.Q=FALSE, varmethod = "tmle",N_time=4))
+  return(res)
+}
+int.end.time <- Sys.time()
+difftime(int.end.time, int.start.time, units="mins")
+
+saveRDS(resdf_noDetQ_tmle_T4, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_tmle_T4.RDS"))
+
+
+
+resdf_noDetQ_Qint_ic_T4 <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[i]], resdf=NULL, Qint=TRUE, det.Q=FALSE, varmethod = "ic",N_time=4))
+  return(res)
+}
+saveRDS(resdf_noDetQ_Qint_ic_T4, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_Qint_ic_T4.RDS"))
+
+
+int.start.time <- Sys.time()
+resdf_ic_T4 <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[i]], resdf=NULL, Qint=FALSE, det.Q=FALSE, varmethod = "ic",N_time=4))
+  return(res)
+}
+int.end.time <- Sys.time()
+difftime(int.end.time, int.start.time, units="mins")
+
+saveRDS(resdf_ic_T4, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_ic_T4.RDS"))
 
 
 
@@ -88,11 +135,19 @@ Ntime=11
 
 #primary
 int.start.time <- Sys.time()
-resdf_noDetQ_Qint_tmle <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+resdf_noDetQ_Qint_tmle_T11 <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
   res <- NULL
   try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[i]], resdf=NULL, Qint=FALSE, det.Q=FALSE, varmethod = "tmle",N_time=11))
   return(res)
 }
-saveRDS(resdf_noDetQ_Qint_tmle, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_tmle_T11.RDS"))
+saveRDS(resdf_noDetQ_Qint_tmle_T11, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_tmle_T11.RDS"))
+
+int.start.time <- Sys.time()
+resdf_noDetQ_Qint_ic_T11 <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet_no_cens(d_wide_list[[i]], resdf=NULL, Qint=FALSE, det.Q=FALSE, varmethod = "ic",N_time=11))
+  return(res)
+}
+saveRDS(resdf_noDetQ_Qint_ic_T11, paste0(here::here(),"/sim_res/null_no_cens_sim_res_noDetQ_ic_T11.RDS"))
 
 
