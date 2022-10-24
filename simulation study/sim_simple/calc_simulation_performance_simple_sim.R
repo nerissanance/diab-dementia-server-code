fd62b13021b9a6e7219aa3505b50e89b2f84b9a8
Andrew Mertens <amertens@berkeley.edu>
2022-10-23 19:16:58 -0700

    update

--- simulation study/sim_simple/calc_simulation_performance_simple_sim.R
+++ simulation study/sim_simple/calc_simulation_performance_simple_sim.R
@@ -5,6 +5,7 @@ rm(list=ls())
 source(here::here("0_config.R"))
 source(paste0(here::here(),"/0_ltmle_Estimate_update.R"))
 source(paste0(here::here(),"/simulation study/0_simulation_functions.R"))
+source(paste0(here::here(),"/simulation study/0_simulation_cleaning_functions.R"))
 
 
 #--------------------------------
@@ -12,44 +13,79 @@ source(paste0(here::here(),"/simulation study/0_simulation_functions.R"))
 #--------------------------------
 
 load(paste0(here::here(),"/sim_res/simulation_results_simple.Rdata"))
-d <- bind_rows(
-  data.frame(resdf_ic_glm, analysis="ic_glm"),
-  data.frame(resdf_tmle_glm, analysis="tmle_glm"),
-  data.frame(resdf_ic_glmnet, analysis="ic_glmnet"),
-  data.frame(resdf_tmle_glmnet, analysis="tmle_glmnet"))
 
-head(d)
+#merge IC and TMLE
+colnames(resdf_ic_glm)
+resdf_ic_glm<- resdf_ic_glm %>% select(estimate, std.dev, CI.2.5., CI.97.5., ate, ate.sd, ate.ci.lb, ate.ci.ub) %>% rename(ic_ci_lb=CI.2.5., ic_ci_ub=CI.97.5., ate_ic_ci_lb=ate.ci.lb, ate_ic_ci_ub=ate.ci.ub)
+resdf_tmle_glm<- resdf_tmle_glm %>% select(CI.2.5., CI.97.5., ate.ci.lb, ate.ci.ub) %>% rename(tmle_ci_lb=CI.2.5., tmle_ci_ub=CI.97.5., ate_tmle_ci_lb=ate.ci.lb, ate_tmle_ci_ub=ate.ci.ub)
+resdf_glm <- cbind(resdf_ic_glm, resdf_tmle_glm) %>% mutate(estimator="GLM", iteration=1:n())
+
+resdf_ic_glmnet<- resdf_ic_glmnet %>% select(estimate, std.dev, CI.2.5., CI.97.5., ate, ate.sd, ate.ci.lb, ate.ci.ub) %>% rename(ic_ci_lb=CI.2.5., ic_ci_ub=CI.97.5., ate_ic_ci_lb=ate.ci.lb, ate_ic_ci_ub=ate.ci.ub)
+resdf_tmle_glmnet<- resdf_tmle_glmnet %>% select(CI.2.5., CI.97.5., ate.ci.lb, ate.ci.ub) %>% rename(tmle_ci_lb=CI.2.5., tmle_ci_ub=CI.97.5., ate_tmle_ci_lb=ate.ci.lb, ate_tmle_ci_ub=ate.ci.ub)
+resdf_glmnet <- cbind(resdf_ic_glmnet, resdf_tmle_glmnet) %>% mutate(estimator="LASSO", iteration=1:n())
+
+
+#merge in bootstrap
 
 #load bootstrap
 boot_iter_files <- dir(path=paste0(here::here(),"/data/sim_simple/bootstrap/"), pattern = "*.RDS")
-boot_iter_files <- boot_iter_files[grepl("sim_res_boot_CV_simple",boot_iter_files)]
-length(boot_iter_files)
-d <- readRDS(paste0(here::here(),"/data/sim_simple/bootstrap/sim_res_boot_CV_simple831.RDS"))
-head(d)
+boot_iter_files_CV <- boot_iter_files[grepl("sim_res_boot_CV_simple_200iter_",boot_iter_files)]
+boot_iter_files_no_ties <- boot_iter_files[grepl("sim_res_boot_without_replacement_simple_200iter_",boot_iter_files)]
+length(boot_iter_files_CV)
+
 setwd(paste0(here::here(),"/data/sim_simple/bootstrap/"))
-boot_res <- boot_iter_files %>% map(readRDS) %>% map_dfr(~bind_rows(.) , .id="boot_iter")
+boot_res_CV <- boot_iter_files_CV %>% map(readRDS) %>% map_dfr(~bind_rows(.) , .id="boot_iter")
+boot_res_no_ties <- boot_iter_files_no_ties %>% map(readRDS) %>% map_dfr(~bind_rows(.) , .id="boot_iter")
 
 #calc bootstrap CI's
-boot_CIs <- boot_res %>% group_by(boot_iter) %>%
+boot_CIs_CV <- boot_res_CV %>%
+  group_by(iteration) %>%
   summarise(
-    CI1=quantile(estimate,.025),
-    CI2=quantile(estimate,.975),
-    ate.CI1=quantile(ate,.025),
-    ate.CI2=quantile(ate,.975)
+    boot_cv_CI1=quantile(estimate,.025),
+    boot_cv_CI2=quantile(estimate,.975),
+    ate.boot_cv_CI1=quantile(ate,.025),
+    ate.boot_cv_CI2=quantile(ate,.975)
 )
-boot_CIs
+boot_CIs_CV
 
-hist(boot_res$ate[boot_res$boot_iter==4])
-hist(log(boot_res$estimate[boot_res$boot_iter==4]))
+boot_res_no_ties <- boot_res_no_ties %>%
+  group_by(iteration) %>%
+  summarise(
+    boot_subsamp_CI1=quantile(estimate,.025),
+    boot_subsamp_CI2=quantile(estimate,.975),
+    ate.boot_subsamp_CI1=quantile(ate,.025),
+    ate.boot_subsamp_CI2=quantile(ate,.975),
+    n=mean(N)
+  ) #%>%
+  #correct CI's
+  # mutate(boot_subsamp_CI1=boot_subsamp_CI1*(sqrt((2/3)*n)/sqrt(n)),
+  #        boot_subsamp_CI2=boot_subsamp_CI2*(sqrt((2/3)*n)/sqrt(n)),
+  #        ate.boot_subsamp_CI1=ate.boot_subsamp_CI1*(sqrt((2/3)*n)/sqrt(n)),
+  #        ate.boot_subsamp_CI2=ate.boot_subsamp_CI2*(sqrt((2/3)*n)/sqrt(n))
+  #        )
+boot_res_no_ties
 
+#NOTE: need to correct with Mark's methods
+mean(boot_CIs_CV$ate.boot_cv_CI2-boot_CIs_CV$ate.boot_cv_CI1)
+mean(boot_res_no_ties$ate.boot_subsamp_CI2-boot_res_no_ties$ate.boot_subsamp_CI1)
+#Why are subsampled narrower?
+
+
+resdf_glmnet <- left_join(resdf_glmnet, boot_CIs_CV, by="iteration")
+resdf_glmnet <- left_join(resdf_glmnet, boot_res_no_ties, by="iteration")
+
+d <- bind_rows(resdf_glm, resdf_glmnet)
+tail(d)
 
 #--------------------------------
 # Set truth
 #--------------------------------
 
+load(paste0(here::here(),"/sim_res/simple_sim_truth.Rdata"))
 
-d$true.RR <- 1
-d$true.RD <- 0
+
+d$true.RR <- simple_sim_trueRR
+d$true.RD <- simple_sim_trueRD
 
 
 
@@ -57,124 +93,47 @@ d$true.RD <- 0
 # Calc performance
 #--------------------------------
 
-d_T4_null <- d
 
-
-perf_tab_RR <- d %>% group_by(analysis) %>%
-  mutate(variance=mean((estimate-mean(estimate))^2), #check if the brackets in this formula are right
-         variance_log=mean((log(estimate)-mean(log(estimate)))^2),
-         RD.variance=mean((mean(ate)-ate)^2),
-         o.ci.lb = estimate - 1.96 * sqrt(variance),
-         o.ci.ub = estimate + 1.96 * sqrt(variance),
-         o.ci.lb2 = log(estimate) - 1.96 * sqrt(variance_log),
-         o.ci.ub2 = log(estimate) + 1.96 * sqrt(variance_log)) %>%
+colnames(d)
+perf_tab_RR <- d %>% group_by(estimator) %>%
+  mutate(variance=mean((log(estimate)-mean(log(estimate)))^2),
+         o.ci.lb = log(estimate) - 1.96 * sqrt(variance),
+         o.ci.ub = log(estimate) + 1.96 * sqrt(variance)) %>%
   summarize(
-    bias_logRR=mean(log(estimate))-log(true.RR),
-    variance_logRR=mean((mean(log(estimate))-log(estimate))^2),
-    mse = bias_logRR^2 + variance_logRR,
-    bias_se_ratio= bias_logRR/sqrt(variance_logRR),
-    coverage=mean(CI.2.5.<=true.RR & true.RR<=CI.97.5.)*100,
-    #oracle coverage
-    o.coverage=mean(o.ci.lb<=true.RR & true.RR<= o.ci.ub)*100,
-    o.coverage_logRR=mean(o.ci.lb2<=log(true.RR) & log(true.RR)<= o.ci.ub2)*100,
-    mean_ci_width_logRR=mean(log(CI.97.5.)-log(CI.2.5.)),
-    power=mean((CI.2.5. > 1 & CI.97.5.>1)|(CI.2.5. < 1 & CI.97.5.<1))*100
-  ) %>%
-  distinct()
-
-
-perf_tab_diff <- d %>% filter(!is.na(ate)) %>%
-  subset(., select = -c(estimate, std.dev, CI.2.5., CI.97.5.)) %>%
-  rename(estimate= ate, std.dev= ate.sd, CI.2.5.=ate.ci.lb, CI.97.5.=ate.ci.ub)%>%
-  group_by(analysis) %>%
-  mutate(variance=mean((estimate-mean(estimate))^2),
-         o.ci.lb = estimate - 1.96 * sqrt(variance),
-         o.ci.ub = estimate + 1.96 * sqrt(variance)) %>%
-  summarize(
-    bias=mean((estimate))-(true.RD),
-    variance=mean((estimate-mean(estimate))^2),
+    bias=mean(log(estimate))-log(true.RR),
+    variance=variance[1],
     mse = bias^2 + variance,
     bias_se_ratio= bias/sqrt(variance),
-    coverage=mean(CI.2.5.<=true.RD & true.RD<=CI.97.5.)*100,
-    #oracle coverage
-    o.coverage=mean(o.ci.lb<=true.RD & true.RD<= o.ci.ub)*100,
-    mean_ci_width=mean((CI.97.5.)-(CI.2.5.)),
-    power=mean((CI.2.5. > 0 & CI.97.5.>0)|(CI.2.5. < 0 & CI.97.5.<0))*100,
-    o.power=mean((o.ci.lb > 0 & o.ci.ub>0)|(o.ci.lb < 0 & o.ci.ub<0))*100
+    o.coverage=mean(o.ci.lb<=log(true.RR) & log(true.RR)<= o.ci.ub)*100,
+    coverage_ic=mean(ic_ci_lb<=true.RR & true.RR<=ic_ci_ub)*100,
+    coverage_tmle=mean(tmle_ci_lb<=true.RR & true.RR<=tmle_ci_ub)*100,
+    coverage_cv_boot=mean(boot_cv_CI1<=true.RR & true.RR<=boot_cv_CI2)*100,
+    coverage_subsamp_boot=mean(boot_subsamp_CI1 <=true.RR & true.RR<=boot_subsamp_CI2)*100#,
+    # mean_ci_width=mean(log(CI.97.5.)-log(CI.2.5.)),
+    # power=mean((CI.2.5. > 1 & CI.97.5.>1)|(CI.2.5. < 1 & CI.97.5.<1))*100
   ) %>%
   distinct()
-
-#calculate bootstrap performance
-perf_tab_RR_boot <- data.frame(
-  analysis = "Bootstrap - clustered ID",
-  coverage = mean(boot_CIs$CI1 <= d$true.RR[1] &  boot_CIs$CI2 >= d$true.RR[1])*100,
-  mean_ci_width_logRR=mean(log(boot_CIs$CI2)-log(boot_CIs$CI1)),
-  power=mean((boot_CIs$CI1 > 1 & boot_CIs$CI2>1)|(boot_CIs$CI1 < 1 & boot_CIs$CI2<1))*100
-  )
-
-perf_tab_diff_boot <- data.frame(
-  analysis = "Bootstrap - clustered ID",
-  coverage = mean(boot_CIs$ate.CI1 <= d$true.RD[1] &  boot_CIs$ate.CI2 >= d$true.RD[1])*100,
-  mean_ci_width=mean((boot_CIs$ate.CI2)-(boot_CIs$ate.CI1)),
-  power=mean((boot_CIs$ate.CI1 > 0 & boot_CIs$ate.CI2>0)|(boot_CIs$ate.CI1 < 0 & boot_CIs$ate.CI2<0))*100
-)
-
-perf_tab_RR_null_T4 <- bind_rows(perf_tab_RR, perf_tab_RR_boot)
-perf_tab_diff_null_T4 <- bind_rows(perf_tab_diff, perf_tab_diff_boot)
+perf_tab_RR
 
 
-#--------------------------------
-# Save data
-#--------------------------------
-save(d_T4_null, perf_tab_RR_null_T4, perf_tab_diff_null_T4, file = paste0(here::here(),"/simulation study/sim_performance_results/T4_null_sim_res.Rdata"))
+colnames(d)
+perf_tab_RD <- d %>% group_by(estimator) %>%
+  mutate(variance=mean(((ate)-mean((ate)))^2),
+         o.ci.lb = (ate) - 1.96 * sqrt(variance),
+         o.ci.ub = (ate) + 1.96 * sqrt(variance)) %>%
+  summarize(
+    bias=mean((ate))-(true.RD),
+    variance=variance[1],
+    mse = bias^2 + variance,
+    bias_se_ratio= bias/sqrt(variance),
+    o.coverage=mean(o.ci.lb<=(true.RD) & (true.RD)<= o.ci.ub)*100,
+    coverage_ic=mean(ate_ic_ci_lb<=true.RD & true.RD<=ate_ic_ci_ub)*100,
+    coverage_tmle=mean(ate_tmle_ci_lb<=true.RD & true.RD<=ate_tmle_ci_ub)*100,
+    coverage_cv_boot=mean(ate.boot_cv_CI1<=true.RD & true.RD<=ate.boot_cv_CI2)*100,
+    coverage_subsamp_boot=mean(ate.boot_subsamp_CI1 <=true.RD & true.RD<=ate.boot_subsamp_CI2)*100#,
+    # mean_ci_width=mean((CI.97.5.)-(CI.2.5.)),
+    # power=mean((CI.2.5. > 1 & CI.97.5.>1)|(CI.2.5. < 1 & CI.97.5.<1))*100
+  ) %>%
+  distinct()
+perf_tab_RD
 
-res <- perf_tab_RR_null_T4 %>% arrange(coverage)
-knitr::kable(res, digits =3)
-
-res_diff <- perf_tab_diff_null_T4 %>% arrange(coverage)
-knitr::kable(res_diff, digits =5)
-
-
-#--------------------------------
-# Make data for CI plot
-#--------------------------------
-
-unique(d$analysis)
-
-
-
-
-#--------------------------------
-# Plots
-#--------------------------------
-
-# RR plots
-set.seed(123)
-ggplot(d, aes(y=analysis, x=estimate)) +
-  geom_jitter(width=0, height=0.05, alpha=0.75) +
-  coord_cartesian(xlim=c(0.4, 2)) +
-  geom_vline(xintercept = 1) +
-  geom_vline(aes(xintercept = true.RR), linetype="dashed") +
-  scale_x_continuous(trans = "log10")
-
-
-ggplot(d, aes(x=estimate)) +
-  facet_wrap(~analysis) +
-  geom_density() +
-  geom_vline(xintercept = 1) +
-  geom_vline(aes(xintercept = true.RR), linetype="dashed") +
-  scale_x_continuous(trans = "log10") + theme_bw()
-
-
-#ATE plots
-set.seed(123)
-ggplot(d, aes(y=analysis, x=ate)) +
-  geom_jitter(width=0, height=0.05, alpha=0.75) +
-  geom_vline(xintercept = 0) +
-  geom_vline(aes(xintercept = true.RD), linetype="dashed")
-
-ggplot(d, aes(x=ate)) +
-  facet_wrap(~analysis) +
-  geom_density() +
-  geom_vline(xintercept = 0) +
-  geom_vline(aes(xintercept = true.RD), linetype="dashed") + theme_bw()
--- simulation study/sim_simple/simulate_simple_data.R
+++ simulation study/sim_simple/simulate_simple_data.R
@@ -17,15 +17,7 @@ generate_data3 = function(n) {
   Y2 = as.numeric(U.Yt2 < plogis(L1-(2*A1)-1))
 
   #L2 = ifelse(Y2==0, A1 + L1 + U.Lt2, NA) # Note this adds censoring NA's
-<<<<<<< HEAD
-<<<<<<< HEAD
   L2 = as.numeric(U.Lt1 <plogis(A1 + L1 + U.Lt2))
-=======
-  L2 =  A1 + L1 + U.Lt2
->>>>>>> b8a879fbe6a71830cba3e7f49e7c904231c09262
-=======
-  L2 =  A1 + L1 + U.Lt2
->>>>>>> b8a879fbe6a71830cba3e7f49e7c904231c09262
   A2 = ifelse(Y2==0, as.numeric(U.At2 < plogis(0.001*(L1+A1+L2))),0)
   Y3 = ifelse(Y2==0, as.numeric(U.Yt3 < plogis((L1-A1+L2-A2)/2)), 1)
   # dataframe with endogenous variables
@@ -39,15 +31,8 @@ set.seed(252)
 n=1000
 simp_sim_data_list <- list()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 for(i in 1:1000){
-=======
-for(i in 1:100){
->>>>>>> b8a879fbe6a71830cba3e7f49e7c904231c09262
-=======
-for(i in 1:100){
->>>>>>> b8a879fbe6a71830cba3e7f49e7c904231c09262
   set.seed(i*12)
   simp_sim_data_list[[i]] <- generate_data3(n)
 }
@@ -59,14 +44,10 @@ d <- simp_sim_data_list[[1]]
 
 #check data
 head(d)
+table(is.na(d))
 table(d$A1)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 table(d$L2)
-=======
->>>>>>> b8a879fbe6a71830cba3e7f49e7c904231c09262
-=======
->>>>>>> b8a879fbe6a71830cba3e7f49e7c904231c09262
 table(d$Y2)
 table(d$A2)
 table(d$Y3)
@@ -84,26 +65,21 @@ generate_counterfactual_data = function(n, A) {
   L1 = U.Lt1
   A1 = A
   Y2 = as.numeric(U.Yt2 < plogis(L1-(2*A1)-1))
-  L2 =  A1 + L1 + U.Lt2
+  L2 = as.numeric(U.Lt1 <plogis(A1 + L1 + U.Lt2))
   A2 = A
   Y3 = ifelse(Y2==0, as.numeric(U.Yt3 < plogis((L1-A1+L2-A2)/2)), 1)
   O = data.frame(L1,A1,Y2,L2,A2,Y3)
   return(O)
 }
 
-#NOTE: need to remove censoring from simulation
-
+set.seed(234)
 df_A1 <- generate_counterfactual_data(1000000, A=1)
 df_A0 <- generate_counterfactual_data(1000000, A=0)
 
 simple_sim_trueRR <- prop.table(table(df_A1$Y3))[2]/prop.table(table(df_A0$Y3))[2]
 simple_sim_trueRD <- prop.table(table(df_A1$Y3))[2]-prop.table(table(df_A0$Y3))[2]
 
-
-
-
-
-
+save(simple_sim_trueRR, simple_sim_trueRD, file=paste0(here::here(),"/sim_res/simple_sim_truth.Rdata"))
 
 
 
