653d0207d8f8f68f32ca284e8af91994e9bcd303
Andrew Mertens <amertens@berkeley.edu>
2022-11-04 17:35:56 -0700

    rerun

--- simulation study/sim_original/run_simulation_test2.R
+++ simulation study/sim_original/run_simulation_test2.R
@@ -33,13 +33,13 @@ id=NULL
 
 
 
-# #Ntime=11
-# resdf_ic_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
-#   res <- NULL
-#   try(res <- run_ltmle_glmnet_test(d=d_wide_list[[i]],  N_time=11))
-#   return(res)
-# }
-# saveRDS(resdf_ic_t11, paste0(here::here(),"/sim_res/sim_res_noDetQ_ic_v3.RDS"))
+#Ntime=11
+resdf_ic_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet_test(d=d_wide_list[[i]],  N_time=11))
+  return(res)
+}
+saveRDS(resdf_ic_t11, paste0(here::here(),"/sim_res/sim_res_noDetQ_ic_v3.RDS"))
 
 resdf_Qint_ic_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
   res <- NULL
@@ -49,12 +49,12 @@ resdf_Qint_ic_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling 
 saveRDS(resdf_Qint_ic_t11, paste0(here::here(),"/sim_res/sim_res_Qint_noDetQ_ic_v3.RDS"))
 
 
-resdf_noDetQ_ic_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+resdf_DetQ_ic_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
   res <- NULL
-  try(res <- run_ltmle_glmnet_test(d=d_wide_list[[i]], det.Q=F, N_time=11))
+  try(res <- run_ltmle_glmnet_test(d=d_wide_list[[i]], det.Q=T, N_time=11))
   return(res)
 }
-saveRDS(resdf_noDetQ_ic_t11, paste0(here::here(),"/sim_res/sim_res_noDetQ_ic_v3.RDS"))
+saveRDS(resdf_DetQ_ic_t11, paste0(here::here(),"/sim_res/sim_res_DetQ_ic_v3.RDS"))
 
 
 #Ntime=11
@@ -138,6 +138,14 @@ resdf_ic_ridge_AUC_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhand
 }
 saveRDS(resdf_ic_ridge_AUC_t11, paste0(here::here(),"/sim_res/sim_res_ridge_AUC_ic_v3.RDS"))
 
+
+#Ntime=11
+resdf_glm_ic_t11  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet_test(d=d_wide_list[[i]],  N_time=11, SL.library="glm"))
+  return(res)
+}
+saveRDS(resdf_glm_ic_t11, paste0(here::here(),"/sim_res/sim_res_noDetQ_glm_ic_v3.RDS"))
 
 # SuperLearner_override_EN_AUC
 # SuperLearner_override_ridge_AUC
