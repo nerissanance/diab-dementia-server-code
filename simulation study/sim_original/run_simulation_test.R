aae8f1f9a08021192cb1c14d0849cab683fe8111
Andrew Mertens <amertens@berkeley.edu>
2022-11-08 12:25:14 -0800

    rerun

--- simulation study/sim_original/run_simulation_test.R
+++ simulation study/sim_original/run_simulation_test.R
@@ -15,106 +15,106 @@ d_wide_list <- readRDS(file=here("data/simulated_data_list.RDS"))
 d_wide_list <- d_wide_list[1:200]
 gc()
 
-d=d_wide_list[[1]]
-N_time = 2
-SL.library = c("SL.glmnet")
-resdf=NULL
-Qint=F
-gcomp=F
-det.Q=T
-gbound = c(0.01, 1)
-override_function=SuperLearner_override
-varmethod = "ic" #variance method
-label=""
-glm=FALSE
-id=NULL
-
-
-run_ltmle_glmnet_test <- function(d, N_time = 3,
-                                       SL.library = c("SL.glmnet"),
-                                       resdf=NULL,
-                                       Qint=F,
-                                       gcomp=F,
-                                       det.Q=T,
-                                       gbound = c(0.01, 1),
-                                       override_function=SuperLearner_override,
-                                       varmethod = "ic", #variance method
-                                       label="",
-                                       glm=FALSE,
-                                       id=NULL){
-
-
-
-warn = getOption("warn")
-options(warn=-1)
-
-#clean competing events
-d <-clean_sim_data(d, N_time=N_time)
-
-if(!is.null(id)){
-  baseline_vars <- c(baseline_vars,"id")
-}
-
-#Use only first N time points
-d <- d %>%
-  dplyr::select(!!(baseline_vars),matches(paste0("_(",paste0(0:(N_time-1),collapse="|"),")$")))
-
-
-spec_ltmle <- spec_analysis_sim(data=d, c(long_covariates,"event_death_"),
-                                baseline_vars, N_time,
-                                Avars=c("glp1_"),
-                                Yvars=c("event_dementia_"),
-                                Cvars=c("censor_"))
-#abar_spec = list(rep(1,N_time-1),rep(0,N_time-1))
-abar_spec = list(rep(1,N_time),rep(0,N_time))
-
-set.seed(12345)
-fit = NULL
-
-qform=NULL
-det.q.fun = NULL
-
-
-package_stub("SuperLearner", "SuperLearner", override_function, {
-  testthatsomemore::package_stub("ltmle", "Estimate", Estimate_override, {
-    try(fit <- ltmle(data=spec_ltmle$data,
-                     Anodes = spec_ltmle$Anodes,
-                     Cnodes = spec_ltmle$Cnodes[-1],
-                     Lnodes = spec_ltmle$Lnodes[spec_ltmle$Lnodes!="event_death_0"],
-                     Ynodes = spec_ltmle$Ynodes[-1],
-                     gbound=gbound,
-                     survivalOutcome = T,
-                     abar = abar_spec,
-                     gcomp=gcomp,
-                     Qform = qform,
-                     estimate.time=F,
-                     deterministic.Q.function = det.q.fun,
-                     SL.library = SL.library,
-                     variance.method = varmethod,
-                     id=id
-    ))
-  })})
-
-
-if(!is.null(fit)){
-  res <- summary(fit)
-  res.iptw <- summary(fit, estimator="iptw")
-  res.RR <- as.data.frame(res$effect.measures$RR)
-  res.ate <- as.data.frame(res$effect.measures$ATE) %>% rename(ate.long.name=long.name,ate=estimate, ate.sd=std.dev , ate.pval=pvalue, ate.ci.lb=CI.2.5., ate.ci.ub=  CI.97.5., ate.log.std.err=log.std.err)
-
-  res.RR.iptw <- as.data.frame(res.iptw$effect.measures$RR) %>% rename(iptw.long.name=long.name, iptw.estimate=estimate, iptw.sd=std.dev , iptw.pval=pvalue, iptw.ci.lb=CI.2.5., iptw.ci.ub=  CI.97.5., iptw.log.std.err=log.std.err)
-  res.ate.iptw <- as.data.frame(res$effect.measures$ATE) %>% rename(iptw.ate.long.name=long.name, iptw.ate=estimate, iptw.ate.sd=std.dev , iptw.ate.pval=pvalue, iptw.ate.ci.lb=CI.2.5., iptw.ate.ci.ub=  CI.97.5., iptw.ate.log.std.err=log.std.err)
-
-  res <- cbind(res.RR, res.ate, res.RR.iptw, res.ate.iptw)
-  res$label <- label
-}
-if(!is.null(resdf)){
-  res <- bind_rows(resdf, res)
-}
-
-options(warn=warn)
-return(res)
-}
+# d=d_wide_list[[1]]
+# N_time = 2
+# SL.library = c("SL.glmnet")
+# resdf=NULL
+# Qint=F
+# gcomp=F
+# det.Q=T
+# gbound = c(0.01, 1)
+# override_function=SuperLearner_override
+# varmethod = "ic" #variance method
+# label=""
+# glm=FALSE
+# id=NULL
+#
+#
+# run_ltmle_glmnet_test <- function(d, N_time = 3,
+#                                        SL.library = c("SL.glmnet"),
+#                                        resdf=NULL,
+#                                        Qint=F,
+#                                        gcomp=F,
+#                                        det.Q=T,
+#                                        gbound = c(0.01, 1),
+#                                        override_function=SuperLearner_override,
+#                                        varmethod = "ic", #variance method
+#                                        label="",
+#                                        glm=FALSE,
+#                                        id=NULL){
+#
+#
+#
+# warn = getOption("warn")
+# options(warn=-1)
+#
+# #clean competing events
+# d <-clean_sim_data(d, N_time=N_time)
+#
+# if(!is.null(id)){
+#   baseline_vars <- c(baseline_vars,"id")
+# }
+#
+# #Use only first N time points
+# d <- d %>%
+#   dplyr::select(!!(baseline_vars),matches(paste0("_(",paste0(0:(N_time-1),collapse="|"),")$")))
+#
+#
+# spec_ltmle <- spec_analysis_sim(data=d, c(long_covariates,"event_death_"),
+#                                 baseline_vars, N_time,
+#                                 Avars=c("glp1_"),
+#                                 Yvars=c("event_dementia_"),
+#                                 Cvars=c("censor_"))
+# #abar_spec = list(rep(1,N_time-1),rep(0,N_time-1))
+# abar_spec = list(rep(1,N_time),rep(0,N_time))
+#
+# set.seed(12345)
+# fit = NULL
+#
+# qform=NULL
+# det.q.fun = NULL
+#
+#
+# package_stub("SuperLearner", "SuperLearner", override_function, {
+#   testthatsomemore::package_stub("ltmle", "Estimate", Estimate_override, {
+#     try(fit <- ltmle(data=spec_ltmle$data,
+#                      Anodes = spec_ltmle$Anodes,
+#                      Cnodes = spec_ltmle$Cnodes[-1],
+#                      Lnodes = spec_ltmle$Lnodes[spec_ltmle$Lnodes!="event_death_0"],
+#                      Ynodes = spec_ltmle$Ynodes[-1],
+#                      gbound=gbound,
+#                      survivalOutcome = T,
+#                      abar = abar_spec,
+#                      gcomp=gcomp,
+#                      Qform = qform,
+#                      estimate.time=F,
+#                      deterministic.Q.function = det.q.fun,
+#                      SL.library = SL.library,
+#                      variance.method = varmethod,
+#                      id=id
+#     ))
+#   })})
+#
+#
+# if(!is.null(fit)){
+#   res <- summary(fit)
+#   res.iptw <- summary(fit, estimator="iptw")
+#   res.RR <- as.data.frame(res$effect.measures$RR)
+#   res.ate <- as.data.frame(res$effect.measures$ATE) %>% rename(ate.long.name=long.name,ate=estimate, ate.sd=std.dev , ate.pval=pvalue, ate.ci.lb=CI.2.5., ate.ci.ub=  CI.97.5., ate.log.std.err=log.std.err)
+#
+#   res.RR.iptw <- as.data.frame(res.iptw$effect.measures$RR) %>% rename(iptw.long.name=long.name, iptw.estimate=estimate, iptw.sd=std.dev , iptw.pval=pvalue, iptw.ci.lb=CI.2.5., iptw.ci.ub=  CI.97.5., iptw.log.std.err=log.std.err)
+#   res.ate.iptw <- as.data.frame(res$effect.measures$ATE) %>% rename(iptw.ate.long.name=long.name, iptw.ate=estimate, iptw.ate.sd=std.dev , iptw.ate.pval=pvalue, iptw.ate.ci.lb=CI.2.5., iptw.ate.ci.ub=  CI.97.5., iptw.ate.log.std.err=log.std.err)
+#
+#   res <- cbind(res.RR, res.ate, res.RR.iptw, res.ate.iptw)
+#   res$label <- label
+# }
+# if(!is.null(resdf)){
+#   res <- bind_rows(resdf, res)
+# }
+#
+# options(warn=warn)
+# return(res)
+# }
 
 #Ntime=2
 resdf_ic_t2  <- foreach(i = 1:200, .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
