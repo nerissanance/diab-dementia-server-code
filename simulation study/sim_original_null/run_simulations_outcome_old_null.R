445a0bd815c82f292ffc714cf8781c72ef22db0f
Andrew Mertens <amertens@berkeley.edu>
2023-04-21 07:26:24 -0700

    sim update

--- simulation study/sim_original_null/run_simulations_outcome_old_null.R
+++ simulation study/sim_original_null/run_simulations_outcome_old_null.R
@@ -158,6 +158,40 @@ gc()
 saveRDS(resdf_EN, paste0(here::here(),"/sim_res/old_null_sim_res_EN_ic_T11.RDS"))
 
 
+#lasso prescreen
+resdf_Qint_noDetQ_lasso_prescreen <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=TRUE, det.Q=FALSE, varmethod = "ic", override_function=SuperLearner_override_lasso_prescreen))
+
+  return(res)
+}
+saveRDS(resdf_Qint_noDetQ_lasso_prescreen, paste0(here::here(),"/sim_res/old_null_sim_res_ic_Qint_noDetQ_lasso_prescreen_T11.RDS"))
+
+#This ones running forever
+# resdf_Qint_noDetQ_lasso_prescreen <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+#   res <- NULL
+#   try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=TRUE, det.Q=TRUE, varmethod = "ic", override_function=SuperLearner_override_lasso_prescreen))
+#
+#   return(res)
+# }
+# saveRDS(resdf_Qint_noDetQ_lasso_prescreen, paste0(here::here(),"/sim_res/old_null_sim_res_ic_Qint_DetQ_lasso_prescreen_T11.RDS"))
+#
+
+resdf_AUC <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=FALSE, varmethod = "ic",override_function=SuperLearner_override_AUC))
+  return(res)
+}
+saveRDS(resdf_AUC, paste0(here::here(),"/sim_res/old_null_sim_res_AUC.RDS"))
+
+
+resdf_Qint_AUC <- foreach(i = 1:length(d_wide_list), .combine = 'bind_rows', .errorhandling = 'remove') %dopar% {
+  res <- NULL
+  try(res <- run_ltmle_glmnet(d_wide_list[[i]], resdf=NULL, Qint=TRUE, varmethod = "ic",override_function=SuperLearner_override_AUC))
+  return(res)
+}
+saveRDS(resdf_Qint_AUC, paste0(here::here(),"/sim_res/old_null_sim_res_Qint_AUC.RDS"))
+
 
 #primary
 int.start.time <- Sys.time()
